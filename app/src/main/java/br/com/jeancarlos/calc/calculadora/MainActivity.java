51325378d42ebd910d6f45f9960f7f51197ca5f2
jeancarlos <jeancs.patricio@gmail.com>
2017-06-13 09:47:07 -0300

    Adicionando lógica da calculadora

--- app/src/main/java/br/com/jeancarlos/calc/calculadora/MainActivity.java
+++ app/src/main/java/br/com/jeancarlos/calc/calculadora/MainActivity.java
@@ -1,12 +1,12 @@
 package br.com.jeancarlos.calc.calculadora;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-@SuppressWarnings("SpellCheckingInspection")
+@SuppressWarnings({"SpellCheckingInspection", "PointlessBooleanExpression"})
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btnSum;
@@ -16,9 +16,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button btnTwo;
     private Button btnOne;
     private Button btnResult;
+    private Button btnMult;
     private TextView textResult;
 
     private int result;
+    private boolean isSum = false;
+    private boolean isSub = false;
+    private boolean isMult = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +36,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnFour = (Button) findViewById(R.id.button_four);
         btnSum = (Button) findViewById(R.id.button_sum);
         btnSub = (Button) findViewById(R.id.button_sub);
+        btnMult = (Button) findViewById(R.id.button_mult);
         btnResult = (Button) findViewById(R.id.button_result);
 
         // Coloca o evento de clique para cada botão
@@ -41,6 +46,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnThree.setOnClickListener(this);
         btnFour.setOnClickListener(this);
         btnSum.setOnClickListener(this);
+        btnMult.setOnClickListener(this);
         btnSub.setOnClickListener(this);
         btnResult.setOnClickListener(this);
     }
@@ -49,12 +55,112 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         // Pega o texto atual do resultado
         String currentText = textResult.getText().toString();
+        int buttonId = v.getId();
 
-        switch (v.getId()) {
+        switch (buttonId) {
             case R.id.button_one:
-                result = result + 1;
                 textResult.setText(currentText + "1");
                 break;
+
+            case R.id.button_two:
+                textResult.setText(currentText + "2");
+                break;
+
+            case R.id.button_three:
+                textResult.setText(currentText + "3");
+                break;
+
+            case R.id.button_four:
+                textResult.setText(currentText + "4");
+                break;
+
+            case R.id.button_result:
+                String result = calculate(currentText);
+                clear();
+                textResult.setText(result);
+                break;
         }
+
+        if (temMaisQueUmOperador(currentText) == false) {
+            switch (buttonId) {
+                case R.id.button_sum:
+                    isSum = true;
+                    textResult.setText(currentText + "+");
+                    break;
+
+                case R.id.button_sub:
+                    isSub = true;
+                    textResult.setText(currentText + "-");
+                    break;
+
+                case R.id.button_mult:
+                    isMult = true;
+                    textResult.setText(currentText + "*");
+                    break;
+            }
+        }
+    }
+
+    /**
+     * Calcula o resultado
+     *
+     * @param currentResult Valor atual do texto de resultado
+     * @return Resulta da operação
+     */
+    private String calculate(String currentResult) {
+        int calc;
+
+        if (isSum) {
+            String[] numeros = currentResult.split("\\+");
+            calc = Integer.parseInt(numeros[0]) + Integer.parseInt(numeros[1]);
+
+        } else if (isSub) {
+            String[] numeros = currentResult.split("\\-");
+            calc = Integer.parseInt(numeros[0]) - Integer.parseInt(numeros[1]);
+
+        } else {
+            String[] numeros = currentResult.split("\\*");
+            calc = Integer.parseInt(numeros[0]) * Integer.parseInt(numeros[1]);
+        }
+
+        return String.valueOf(calc);
+    }
+
+
+    /**
+     * Verifica se existe mais de um operador no resultado
+     *
+     * @param currentResult Valor atual do texto de resultado
+     */
+    private boolean temMaisQueUmOperador(String currentResult) {
+        int contador = 1;
+
+        if (currentResult.contains("+")) {
+            contador = contador + 1;
+        }
+
+        if (currentResult.contains("-")) {
+            contador = contador + 1;
+        }
+
+        if (currentResult.contains("*")) {
+            contador = contador + 1;
+        }
+
+        if (contador > 1) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Limpa o texto do resultado e reseta as variáveis
+     */
+    private void clear() {
+        textResult.setText("");
+        isSum = false;
+        isSub = false;
+        isMult = false;
     }
 }
